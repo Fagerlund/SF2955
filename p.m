37523a2a99d54f1125f058316224f32b84b467eb
Axenmagnus <68119345+Axenmagnus@users.noreply.github.com>
2022-04-18 13:31:32 +0200

    Add files via upload

---
+++ p.m
@@ -0,0 +1,12 @@
+function val = p(x1,x2,y)
+
+    load("stations.mat"); %Maybe not efficient to load each time? Make global
+    v=90; zeta=1.5; gamma=3; %Constatns
+    xs=zeros(12,length(x1)); % Placeholders
+    xs(1:6,:)=bsxfun(@minus,x1,transpose(pos_vec(1,:)));
+    xs(7:12,:)=bsxfun(@minus,x2,transpose(pos_vec(2,:)));
+
+
+    val=prod(normpdf(y,v-10*gamma*log10(sqrt(xs(1:6,:).^2+xs(7:12,:).^2)),zeta));
+end
+
